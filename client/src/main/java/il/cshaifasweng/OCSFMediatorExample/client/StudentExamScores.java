a5eb51dce71936563d725b9fd5e5d0dca72b4209
stavi <ssolonik@campus.haifa.ac.il>
2023-06-17 10:52:43 +0300

    student pages were added (mainly gui): 1. primary controller 2. view exam scores 3. start exam Test class in server was added - only to manage a test using server (not gui)

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/StudentExamScores.java
@@ -0,0 +1,68 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import il.cshaifasweng.OCSFMediatorExample.entities.Student;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.control.TableColumn;
+
+
+import java.io.IOException;
+
+// TODO: get the student grades (need the name)
+
+public class StudentExamScores {
+    @FXML
+    private TableColumn<?, ?> exam_scores_table;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Label student_id;
+
+    @FXML
+    private Label student_name;
+
+    @FXML
+    private Button go_back_button;
+
+    private Student student;
+
+    @FXML
+    void initialize() {
+        String filename = "100-sample-contactslist-excel.csv";
+        String[] columns = {"first_name",
+                "last_name", "phone", "email"};
+//        List<DataRow> data = readCSVFile(filename,
+//                columns);
+//        first_name.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getFirst_name()));
+//        last_name.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getLast_name()));
+//        phone.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getPhone()));
+//        email.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getEmail()));
+//        contactsTable.setItems(FXCollections.observableList(data));
+    }
+
+    @FXML
+    void viewLastPage(ActionEvent event) {
+        // Go to "student_primary.fxml"
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("student_primary.fxml"));
+            Parent root = loader.load();
+            Scene nextScene = new Scene(root);
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.setScene(nextScene);
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/StudentPrimaryController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/StudentPrimaryController.java
@@ -1,32 +1,93 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
+import il.cshaifasweng.OCSFMediatorExample.entities.Student;
+
+// TODO: get the student details and pass them to child pages
 
 public class StudentPrimaryController {
+
+    @FXML
+    private Button exams_scores_button;
+
+    @FXML
+    private Button go_back_button;
+
     @FXML
     private AnchorPane pane;
+
     @FXML
-    private TextField codeTF;
+    private Button start_exams_button;
+
     @FXML
-    private Button codeBtn;
+    private Label student_id;
+
+    @FXML
+    private Label student_name;
+
     @FXML
     void initialize(){
+        //EventBus.getDefault().register(this);
+        String temp_id = "123456789";
+        String temp_name = "Stav";
 
+        student_id.setText(temp_id);
+        student_name.setText(temp_name);
     }
-    @FXML
-    void EnterExam(ActionEvent event) {
-        String code = codeTF.getText();
-        String message = "EnterExam " + code;
-        System.out.println("the message is: " + message);//for debugging
 
-        SimpleClient.sendMessage(message);
+    @FXML
+    void viewExamScores(ActionEvent event) {
+        // Go to "student_exam_scores.fxml"
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("student_exam_scores.fxml"));
+            Parent root = loader.load();
+            Scene nextScene = new Scene(root);
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.setScene(nextScene);
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void viewLastPage(ActionEvent event) {
+        // Go to "log_in.fxml"
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("log_in.fxml"));
+            Parent root = loader.load();
+            Scene nextScene = new Scene(root);
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.setScene(nextScene);
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void viewStartExam(ActionEvent event) {
+        // Go to "student_start_test.fxml"
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("student_start_test.fxml"));
+            Parent root = loader.load();
+            Scene nextScene = new Scene(root);
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.setScene(nextScene);
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
